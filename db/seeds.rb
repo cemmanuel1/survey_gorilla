c9c634012a65e491d860a458c2067e6a0c2d9076
mcarthurgill <mickgill2008@gmail.com>
2013-07-05 15:47:04 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,18 @@ Question.create content: "How many Rowlands do you want in your life", survey_id
 Choice.create answer: "all of them", question_id:2
 Choice.create answer: "all of them and then some", question_id:2
 Choice.create answer: "all of them plus one", question_id:2
+
+
+User.create name: "Rowland", email: "rowland@gmail.com", password: "password"
+User.create name: "Zee", email: "zee@gmail.com", password: "password"
+
+Survey.create title: "questions for best friends", creator_id: 3
+Question.create content: "What is your favorite color", survey_id: 2
+Choice.create answer: "Red", question_id: 3
+Choice.create answer: "Blue", question_id: 3
+Choice.create answer: "Green", question_id: 3
+Question.create content: "What is your favorite place to eat", survey_id: 2
+Choice.create answer: "Subway", question_id:4
+Choice.create answer: "Chipotle", question_id:4
+Choice.create answer: "Hot dog stand", question_id:4
+
