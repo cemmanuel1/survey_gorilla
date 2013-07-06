b47050f51acca9372f234dea5d2daf344a20949f
dbc-warrior <apprentice@devbootcamp.com>
2013-07-05 17:33:01 -0700

    we can add response

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -7,8 +7,15 @@ get '/survey/new' do
 	erb :"surveys/create_survey"
 end
 
+post '/survey/new' do
+  erb :"surveys/create_survey"
+end
+
 get '/survey/:survey_id' do |id|
   @survey = Survey.find(id)
   erb :"surveys/show_survey"
 end
 
+get '/load_partial' do 
+  erb :"surveys/_question_form", layout: false
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+ $("#add_question").on('submit', function(e) {
+  e.preventDefault();
+  $.get('/load_partial', function(response) {
+    $('.big_divver').append(response);
+  });
+ });
 });
