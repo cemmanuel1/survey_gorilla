0f0c0990b5436846c829d8752d8cde70993e9787
mcarthurgill <mickgill2008@gmail.com>
2013-07-06 12:46:46 -0700

    added alert

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	alert("hey everyone...so i have basically the creating a survey part. It is creating new objects and saving them in the database too. i am working from home and on my own branch, but i havent pushed anything to github yet. There are still a few things I need to do before i push. If you want to work on this, I'll come meet you at DBC. Just text me and let me know. My number is 334.399.4374. -McArthur");
  $("#add_question").on('submit', function(e) {
   e.preventDefault();
   $.get('/load_partial', function(response) {
