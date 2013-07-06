126627bb219a7f290e9515d858365f0f7595ef32
mcarthurgill <mickgill2008@gmail.com>
2013-07-06 11:30:19 -0700

    need to serialize everything in big divver

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -8,7 +8,9 @@ get '/survey/new' do
 end
 
 post '/survey/new' do
-  erb :"surveys/create_survey"
+	survey = Survey.create(title: params[:title], creator_id: session[:user_id])
+	session[:survey_id] = survey.id
+  #erb :"surveys/create_survey"
 end
 
 get '/survey/:survey_id' do |id|
@@ -16,10 +18,20 @@ get '/survey/:survey_id' do |id|
   erb :"surveys/show_survey"
 end
 
-get '/load_partial' do 
-  erb :"surveys/_question_form", layout: false
+post '/complete_question' do
+	if params
+		Question.create(suvey_id: session[:survey_id], content: params[:question_made])
+	end
+end
+
+get '/load_form_partial' do
+	erb :"surveys/_question_form", layout: false
 end
 
 get '/load_response_partial' do
   erb :"surveys/_response_form", layout: false
+end
+
+post '/finalize_survey' do
+	#here we have to watch out for creating that last question
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,44 @@
 $(document).ready(function() {
- $("#add_question").on('submit', function(e) {
-  e.preventDefault();
-  $.get('/load_partial', function(response) {
-    $('.big_divver').append(response);
-  });
+	$('#begin_survey').on("submit", function(e){
+		e.preventDefault();
+		$.post('/survey/new', function(){
+			$('#begin_survey').serialize();
+		}).done(function(){
+			$('.survey_creation').fadeOut("slow").empty();
+			$.get('/load_form_partial', function(response){
+				$('.big_divver').append(response);
+			});
+		});
+	});
+	$('.big_divver').on("submit", '.add_new_response', function(e){
+		e.preventDefault();
+		$.get('/load_response_partial', function(response){
+			$('.big_divver').append(response);
+		});
+	});
+	$('.big_divver').on("submit", '.submit_question', function(e){
+		e.preventDefault();
+		console.log($('.add_new_response').serialize());
+		// $.post('/complete_question', function(){
+		// 	console.
+		// });
+	});
 });
- $(".big_divver").on('submit', '.add_response' ,function(e) {
-  e.preventDefault();
-  $.get('/load_response_partial', function(response){
-    $('.big_divver').append(response);
-  });
-});
-});
+
+
+
+
+
+
+// $("#add_question").on('submit', function(e) {
+//   e.preventDefault();
+//   $.get('/load_partial', function(response) {
+//     $('.big_divver').append(response);
+//   });
+// });
+//  $(".big_divver").on('submit', '.add_response' ,function(e) {
+//   e.preventDefault();
+//   $.get('/load_response_partial', function(response){
+//   	$('.add_response').closest('.added_question').append(response);
+//   });
+// });
